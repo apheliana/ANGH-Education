7f01d08d3e61823404eca65f9c447ba8c6b57d14
apheliana <65423514+apheliana@users.noreply.github.com>
2020-05-18 01:52:27 +0300

    Update data-types.component.ts

--- src/app/data-types.component.ts
+++ src/app/data-types.component.ts
@@ -5,13 +5,48 @@ import { Component } from '@angular/core';
 })
 export class DataTypesComponent {
   constructor() {
-    const parent = new Parent();
-    console.log('parent', parent);
+    const fatih = new Parent();
+    fatih.name = 'Fatih';
+    fatih.age = 38;
+
+    const ada = new Child();
+    ada.age = 6;
+    ada.name = 'Ada';
+    fatih.children.push(ada);
+    fatih.luckyNumbers = [1, 3, 5, 7];
+
+    console.log('parent', fatih);
+
+    const calculateSumOfLuckyNumbers = fatih.calculateSumOfLuckyNumbers();
+    console.log('calculateSumOfLuckyNumbers', calculateSumOfLuckyNumbers);
   }
+}
+
+class Address {
+  city: string = '';
+  country: string = '';
+}
+
+class Company {
+  name: string = '';
+  address: Address = new Address();
 }
 
 class Parent {
   name: string = '';
   age: number = 0;
-  isSmoking: boolean = false;
+  address: Address = new Address();
+  children: Child[] = [];
+  luckyNumbers: number[] = [];
+
+  calculateSumOfLuckyNumbers(): number {
+    let total = 0;
+    this.luckyNumbers.forEach(number => total += number);
+    return total;
+  }
+}
+
+class Child {
+  name: string = '';
+  age: number = 0;
 }
