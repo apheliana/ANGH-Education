0ee17a114eb2bbf06069f4628cfa89c93b99e55e
apheliana <65423514+apheliana@users.noreply.github.com>
2020-05-19 00:35:15 +0300

    select-update-reset

--- src/app/form-practise.component.ts
+++ src/app/form-practise.component.ts
@@ -7,6 +7,7 @@ export class FormPractiseComponent {
   personsname='';
   personsage=0;
   aile: Person[] = [];
+  selectedperson:Person=null;
 
   enterPersonsInfo(): void {
     
@@ -15,19 +16,36 @@ export class FormPractiseComponent {
       ebeveyn.age = this.personsage;
     
       this.aile.push(ebeveyn);
+      this.reset();
 
     console.log('PersonsInformation',this.aile);
+
   }
 
   deleteRow(person: Person): void {
     console.log('silinecek kisi: ' + person.name);
     this.aile = this.aile.filter(item => item !== person);
       }
+  
   selectRow(person: Person): void{
     this.personsname=person.name;
     this.personsage=person.age;
+    this.selectedperson = person;
   }
-    }
+  updatePersonsInfo(): void {
+    this.selectedperson.name=this.personsname;
+    this.selectedperson.age=this.personsage;
+    this.reset();
+  }
+  cancelPersonsInfo(): void {
+    this.reset();
+  }
+  private reset(): void {
+    this.personsname='';
+    this.personsage=0;
+    this.selectedperson=null;
+  }
+}
 
 
 
