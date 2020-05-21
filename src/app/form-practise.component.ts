ac5fe0bbe112f8034a6035888f3e8caa99d4243b
apheliana <65423514+apheliana@users.noreply.github.com>
2020-05-21 21:19:16 +0300

    css - birth date - calculate age
    calculate age failed

--- src/app/form-practise.component.ts
+++ src/app/form-practise.component.ts
@@ -1,15 +1,30 @@
 import { Component } from '@angular/core';
 
 @Component({
-  templateUrl: './form-practise.component.html'
+  templateUrl: './form-practise.component.html',
+  styleUrls: ['./form-practise.component.css'],
 })
 export class FormPractiseComponent {
   personsname='';
   personsage=0;
   aile: Person[] = [];
-  selectedperson:Person=null;
+  selectedperson:Person = null;
   viewState: 'add' | 'update' = 'add';
-  ifNameEmpty: boolean = false;
+  errorMessage: string = '';
+  birthDate='';
+  birthday: Date = new Date();
+  
+   parseDate(birthDate) {
+    var parts = birthDate.match(/(\d+)/g);
+    // new Date(year, month [, date [, hours[, minutes[, seconds[, ms]]]]])
+    return new Date(parts[0], parts[1]-1, parts[2]); // months are 0-based
+  }
+
+  calculateAge(birthday) { // birthday is a date
+    var ageDifMs = Date.now() - birthday.getTime();
+    var ageDate = new Date(ageDifMs); // miliseconds from epoch
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+  }
 
   constructor() {
     const aileJSON = localStorage.getItem('aile');
@@ -20,20 +35,18 @@ export class FormPractiseComponent {
   }
 
   enterPersonsInfo(): void {
-    if (this.validateForm()==false) {
-      this.ifNameEmpty = true;
-    } else{
+    if (this.validateForm()) {
+
       const ebeveyn = new Person();
       ebeveyn.name = this.personsname;
       ebeveyn.age = this.personsage;
-    
+      ebeveyn.bdate = this.parseDate(this.birthDate);
+      ebeveyn.calculatedAge = this.calculateAge(this.birthday);
+      
       this.aile.push(ebeveyn);
       this.reset();
       this.save();
-
-    console.log('PersonsInformation',this.aile);
     }
-
   }
 
   deleteRow(person: Person): void {
@@ -49,11 +62,15 @@ export class FormPractiseComponent {
     this.viewState='update';
   }
   updatePersonsInfo(): void {
-    this.selectedperson.name=this.personsname;
-    this.selectedperson.age=this.personsage;
-    this.reset();
-    this.save();
-    this.viewState='add';
+    if (this.validateForm()) {
+      this.selectedperson.name=this.personsname;
+      this.selectedperson.age=this.personsage;
+      //this.selectedperson.bdate=;
+     
+      this.reset();
+      this.save();
+      this.viewState='add';
+    }
   }
   cancelPersonsInfo(): void {
     this.reset();
@@ -63,21 +80,27 @@ export class FormPractiseComponent {
     this.personsname='';
     this.personsage=0;
     this.selectedperson=null;
+    this.birthDate='';
   }
   private save(): void {
     localStorage.setItem('aile', JSON.stringify(this.aile));
   }
 
   private validateForm(): boolean {
-    if (this.personsname==null) {
-      return true;
-    } else {
-      return false;
+    this.errorMessage = '';
+    if (this.personsname === '') {
+      this.errorMessage = ' * Name can not be empty * ';
     }
+    if (Number(this.personsage) === 0) {
+      this.errorMessage += ' * Age can only be positive numbers * ';
+    }
+    return this.errorMessage.length === 0
   }
 }
 
 class Person {
   name: string = '';
   age: number = 0;
+  bdate: Date = new Date();
+  calculatedAge: number = 0;
 }
